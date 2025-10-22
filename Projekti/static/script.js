7e8342468aa13476763a895845be9bcf57335fc5
Aleksi Kupila <aleksi.kupila@gmail.com>
2025-10-22 03:07:15 +0300

    Separate spinners for each form, made spinner a bit smaller

--- Projekti/static/script.js
+++ Projekti/static/script.js
@@ -1,28 +1,51 @@
-const suggest_form = document.getElementById('suggest_form');
-const execute_form = document.getElementById('execute_form');
-const analysis_form = document.getElementById('analysis_form');
-const responseDiv = document.getElementById('response');
-const spinner = document.getElementById('spinner');
+document.addEventListener('DOMContentLoaded', () => {
+  
+    const suggest_form = document.getElementById('suggest_form');
+    const execute_form = document.getElementById('execute_form');
+    const analysis_form = document.getElementById('analysis_form');
+    const responseDiv = document.getElementById('response');
 
-const forms = [suggest_form, execute_form, analysis_form];
-console.log("Binding forms...");
-document.addEventListener('submit', async (e) => {
-  const form = e.target;
-  if (!['suggest_form', 'execute_form', 'analysis_form'].includes(form.id)) return;
+  const forms = [suggest_form, execute_form, analysis_form];
+  console.log("Binding forms...");
 
-  e.preventDefault();
-  spinner.style.display = 'block';
+  document.addEventListener('submit', async (e) => {
+    const suggest_spinner = document.getElementById('suggest_spinner');
+    const execute_spinner = document.getElementById('execute_spinner');
+    const analysis_spinner = document.getElementById('analysis_spinner');
+    const form = e.target;
+    if (!['suggest_form', 'execute_form', 'analysis_form'].includes(form.id)) return;
 
-  try {
-    const res = await fetch(form.action, {
-      method: 'POST',
-      body: new FormData(form)
-    });
-    const data = await res.json();
-    responseDiv.innerHTML = data.html;
-  } catch (err) {
-    responseDiv.textContent = 'Error: ' + err.message;
-  } finally {
-    spinner.style.display = 'none';
-  }
+    e.preventDefault();
+    
+    if (form.id == "suggest_form"){
+        suggest_spinner.style.display = 'block';
+    }
+    if (form.id == "execute_form"){
+        execute_spinner.style.display = 'block';
+    }
+    if (form.id == "analysis_form"){
+        analysis_spinner.style.display = 'block';
+    }
+
+    try {
+      const res = await fetch(form.action, {
+        method: 'POST',
+        body: new FormData(form)
+      });
+      const data = await res.json();
+      responseDiv.innerHTML = data.html;  
+    } catch (err) {
+      responseDiv.textContent = 'Error: ' + err.message;
+    } finally {
+        if (suggest_spinner){
+            suggest_spinner.style.display = 'none';
+        }
+        if (execute_spinner){
+            execute_spinner.style.display = 'none';
+        }
+        if (analysis_spinner){
+            analysis_spinner.style.display = 'none';
+        }
+    }
+  });
 });
